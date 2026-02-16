94051e92b024e91c0e9697f698b2d22643f1a83f
nemi <dhruvamathisen@gmail.com>
2026-02-16 16:23:17 +0900

    fixed new issues

--- Scripts/Linux-Information-Notification-Scripts/information.sh
+++ Scripts/Linux-Information-Notification-Scripts/information.sh
@@ -91,12 +91,13 @@ if [ "$Wifi_state" = "disconnected" ]; then
   Wifi_fullInfo=" $Wifi_Icon-$RSSI_value $Wifi_state"
 else
   if [[ -z "$iface" ]]; then
-    vpn_status="${vvv}󰝷 "
+    vpn_status=""
+    # vpn_status="${vvv}󰝷 "
   else
     case "$iface" in
       *-NL-*)  vpn_status="${vvv}🇳🇱 NL" ;;
       *-JP-*)  vpn_status="${vvv}🇯🇵 JP" ;;
-      *-CAN-*) vpn_status="${vvv}🇨🇦 CA" ;;
+      *-CA-*) vpn_status="${vvv}🇨🇦 CA" ;;
       *-NOR-*) vpn_status="${vvv}🇳🇴 NO" ;;
       *-US-*) vpn_status="${vvv}🇺🇸 US" ;;
       *-PL-*) vpn_status="${vvv}🇵🇱 PL" ;;
@@ -113,52 +114,71 @@ fi
 # Bluetooth related information: ----------------------------------------------------------------
 
 # If bluetooth is off, it should show in the main notification.
-BT_powered=$(bluetoothctl show | grep "Powered:" | awk '{print $2}')
-if [ "$BT_powered" = "no" ]; then
+BT_powered=$(cat /sys/class/bluetooth/hci0/rfkill*/state 2>/dev/null) # This reads on hardware. bluetoothctl power off doesn't stop bluetooth model radio transmission. sudo rfkill block bluetooth to cease radio transmission.
+# - sudo rfkill block bluetooth
+# - sudo rfkill unblock bluetooth
+#
+BT_state=""
+if [ "$BT_powered" != "1" ]; then
   BT_state="\n  Off"
 else
-  BT_state="\n 󰂲 Disconnected"
-  BT_macs=$(bluetoothctl devices | cut -d ' ' -f 2)
-  Connected_icon=" " 
-  for mac in $BT_macs; do
-    BT_info=$(bluetoothctl info "$mac")
+# Get connected devices using the Heredoc trick
+  connected_devs=$(bluetoothctl << EOF
+devices Connected
+exit
+EOF
+)
 
-    if echo "$BT_info" | grep -q "Connected: yes"; then
-      # If device is connected, clear BT_state so it doesn't show in main notification.
-      BT_state=""
+  # BT_Display_Names=$(echo "$connected_devs" | grep "Device " | cut -d ' ' -f 3- | paste -sd "," -)
+  BT_macs=$(echo "$connected_devs" | grep "Device " | cut -d ' ' -f 2)
 
-      Connected_name=$(echo "$BT_info" | awk -F': ' '/Name:/ {print $2}')
-      Connected_battery=$(echo "$BT_info" | awk -F '[()]' '/Battery Percentage:/ {print $2}')
+  if [ -z "$BT_macs" ]; then
+    BT_state="\n 󰂲 Disconnected"
+  else
+    # BT_state="\n 󰂱 $BT_Display_Names"
+
+    for mac in $BT_macs; do
+      BT_mac_info=$(bluetoothctl << EOF
+info $mac
+exit
+EOF
+)
+      Connected_name=$(echo "$BT_mac_info" | awk -F': ' '/Name:/ {print $2}')
+      Connected_battery=$(echo "$BT_mac_info" | grep "Battery Percentage" | awk -F '[()]' '{print $2}' | tr -d '% ')
+      Device_class=$(echo "$BT_mac_info" | grep "Icon:" | awk '{print $2}')
       stack_tag="BT_$mac"
 
+      case "$Device_class" in
+        audio-headphones|audio-card) DeviceIcon=" " ;;
+        input-mouse)                 DeviceIcon="󰍽 " ;;
+        input-keyboard)              DeviceIcon="󰌌 " ;;
+        phone)                       DeviceIcon=" " ;;
+        *)                           DeviceIcon="󰝷 " ;;
+      esac
+
+      Connected_icon="There's an error?"
+
       if [ -n "$Connected_battery" ]; then
-        if [ "$Connected_battery" -ge 100 ]; then
-          Connected_icon=" "
-        elif [ "$Connected_battery" -ge 80 ]; then
-          Connected_icon=" "
-        elif [ "$Connected_battery" -ge 60 ]; then
-          Connected_icon=" "
-        elif [ "$Connected_battery" -ge 40 ]; then
-          Connected_icon=" "
-        elif [ "$Connected_battery" -ge 20 ]; then
-          Connected_icon=" "
+          if [ "$Connected_battery" -ge 100 ]; then
+            Connected_icon=" "
+          elif [ "$Connected_battery" -ge 80 ]; then
+            Connected_icon=" "
+          elif [ "$Connected_battery" -ge 60 ]; then
+            Connected_icon=" "
+          elif [ "$Connected_battery" -ge 40 ]; then
+            Connected_icon=" "
+          elif [ "$Connected_battery" -ge 20 ]; then
+            Connected_icon=" "
         fi
-        DeviceType=$(echo "$BT_info" | grep "Icon:" | awk '{print $2}')
-        case "$DeviceType" in
-          audio-headphones|audio-card) DeviceIcon=" " ;;
-          input-mouse)                 DeviceIcon="󰍽 " ;;
-          input-keyboard)              DeviceIcon="󰌌 " ;;
-          phone)                       DeviceIcon=" " ;;
-          *)                           DeviceIcon="󰝷 " ;;
-        esac
-        dunstify -u low -h string:x-dunst-stack-tag:"$stack_tag" -h int:value:"$Connected_battery" "󰂱 $Connected_name $DeviceIcon" "$Connected_icon $Connected_battery%"
-      else
-        # Device is connected but has no battery reported
-        dunstify -u low -h string:x-dunst-stack-tag:"$stack_tag" "󰂱 $Connected_name $DeviceIcon" "Connected"
+
+          dunstify -u low -h string:x-dunst-stack-tag:"$stack_tag" -h int:value:"$Connected_battery" "󰂱 $Connected_name $DeviceIcon" "$Connected_icon $Connected_battery%"
+        else
+          dunstify -u low -h string:x-dunst-stack-tag:"$stack_tag" "󰂱 $Connected_name $DeviceIcon" "Connected"
       fi
-    fi
-  done
+    done
+  fi 
 fi
+
 
 # Just realised, this "string:x-dunst-stack-tag" part just assigns the notificatios "tag". In other, here it just prevents dunst from showing how many times this notification was called. Doesn't actually do anything in terms of the content of the notification.
 # dunstify -h string:x-dunst-stack-tag:"$power" -h int:value:"$power" "$(date +"%b %d %a %H:%M")" "$statusIcon $icon: $power% \n Wifi: $Wifi_state $Wifi_SSID $Wifi_RSSI"
